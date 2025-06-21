d1638c9a5a74388f871f610cc3bc38d0a45fd5f7
ShivRamKrishna <shivramkrishna@ShivRamKrishna.local>
2025-06-21 12:14:36 +0530

    Some Changes

--- apps/recuiter-app/src/components/ui/auth/AuthPassword.tsx
+++ apps/recuiter-app/src/components/ui/auth/AuthPassword.tsx
@@ -19,20 +19,23 @@ const AuthPassword: React.FC<PasswordFieldProps> = ({
         <div className="flex flex-col w-full">
             <div className="flex flex-col gap-2 w-full">
                 <AuthLabel label={label} />
-                <Password
-                    {...rest}
-                    pt={{
-                        input: {
-                            className:
-                            "!w-full !text-black !bg-field border-none focus:!shadow-none !rounded-xl",
-                        },
-                        hideIcon: { className: "!cursor-pointer" },
-                        showIcon: { className: "!cursor-pointer" },
-                    }}
-                    placeholder={placeholder}
-                    toggleMask
-                    feedback={false}
-                />
+                <div className="relative w-full">
+                    <Password
+                        {...rest}
+                        pt={{
+                            input: {
+                                className:
+                                    "!w-full !text-black !bg-field border-none focus:!shadow-none !rounded-xl pr-12", // add padding-right for icon space
+                            },
+                            hideIcon: { className: "!cursor-pointer absolute right-2 top-1/2 -translate-y-1/2" },
+                            showIcon: { className: "!cursor-pointer absolute right-2 top-1/2 -translate-y-1/2" },
+                        }}
+                        className="w-full"
+                        placeholder={placeholder}
+                        toggleMask
+                        feedback={false}
+                    />
+                </div>
             </div>
             {errorMsg && (<small className="w-full text-red-400">{errorMsg?.message}</small>)}
         </div>
