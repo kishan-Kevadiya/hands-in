8ed24564269aaa6de392aa314b26728fb4e441f6
LAPTOP-JN624D9T\Kishan <kishankevadiya12345@gmai..com>
2025-08-15 19:35:59 +0530

    design changes

--- apps/candidate-app/src/pages/landing-page/LandingPage.tsx
+++ apps/candidate-app/src/pages/landing-page/LandingPage.tsx
@@ -5,21 +5,21 @@ import { ReactLenis } from "lenis/react";
 import { useEffect } from "react";
 
 const LandingPage = () => {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-    return (
-        <ReactLenis root>
-            <div className="font-manrope flex flex-col ">
-                <Navbar />
-                <Outlet />
-                {/* <Footer /> */}
-            </div>
-        </ReactLenis>
-    );
+  return (
+    <ReactLenis root>
+      <div className="font-manrope flex flex-col ">
+        <Navbar />
+        <Outlet />
+        {/* <Footer /> */}
+      </div>
+    </ReactLenis>
+  );
 };
 
 export default LandingPage;
