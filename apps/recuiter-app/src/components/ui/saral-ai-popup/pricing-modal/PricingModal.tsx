21dc4c982b98b3349ae31d39959ec0d2e5495e62
Your New Name <you@example.com>
2025-08-28 18:02:04 +0530

    changes

--- apps/recuiter-app/src/components/ui/saral-ai-popup/pricing-modal/PricingModal.tsx
+++ apps/recuiter-app/src/components/ui/saral-ai-popup/pricing-modal/PricingModal.tsx
@@ -163,6 +163,129 @@ export function PricingModal({ isOpen, onClose }: PricingModalProps) {
           </div>
         ))}
       </div>
+
+      
     </CommonModal>
   );
 }
+
+
+
+// export function PricingModal({ isOpen, onClose }: PricingModalProps) {
+//   return (
+//     <CommonModal isOpen={isOpen} onClose={onClose} size="full">
+//       {/* Header */}
+//       <div className="text-center pt-2 sm:pt-4 pb-2 sm:pb-4 mb-2 sm:mb-4 px-2 sm:px-4">
+//         <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-[#3F1562] mb-1 sm:mb-2">
+//           Upgrade Your Plan
+//         </h2>
+//         <p className="text-[#3D1562]/50 text-xs sm:text-sm md:text-base">
+//           Choose the perfect plan for your recruiting needs
+//         </p>
+//       </div>
+
+//       {/* Pricing Grid */}
+// <div className="w-[65vw] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-3 md:gap-4 lg:gap-6 px-2 sm:px-3 md:px-4 lg:px-6 pb-3 sm:pb-4 md:pb-6 max-w-full overflow-hidden">
+// {plans.map((plan, idx) => (
+//   <div
+//     key={plan.name}
+//     className={`
+//       relative rounded-lg md:rounded-xl p-3 sm:p-4 lg:p-5 transition-all 
+//       h-auto xl:h-[620px]  /* only fixed height on XL screens */
+//       ${plan.popular
+//         ? "bg-gradient-to-br from-purple-50 to-pink-50 border-2 border-[#3F1562]/50 shadow-lg xl:scale-105"
+//         : "bg-white border border-gray-200 hover:shadow-lg"
+//       }
+//     `}
+//   >
+//     {/* Content Wrapper */}
+//     <div className="h-auto xl:h-[90%] flex flex-col justify-between">
+//       {/* Most Popular Badge */}
+//       {plan.popular && (
+//         <div className="absolute -top-2 left-1/2 transform -translate-x-1/2">
+//           <span
+//             className="
+//               inline-flex items-center justify-center
+//               bg-[#3F1562] text-white 
+//               px-1.5 py-0.5 sm:px-2 sm:py-0.5 md:px-2.5 md:py-1 
+//               rounded-full 
+//               text-[9px] sm:text-[10px] md:text-xs lg:text-sm xl:text-sm
+//               font-semibold tracking-wide whitespace-nowrap
+//             "
+//           >
+//             MOST POPULAR
+//           </span>
+//         </div>
+//       )}
+
+//       {/* Price */}
+//       <div className="mb-2 sm:mb-3 mt-1 sm:mt-2">
+//         <div className="text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-5xl font-bold text-[#231D4F]">
+//           {plan.price}{" "}
+//           <span className="text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-base font-normal text-[#3D1562]/40">
+//             /month
+//           </span>
+//         </div>
+//         {plan.oldPrice && plan.discount && (
+//           <div className="flex items-center justify-center gap-1 sm:gap-2 mt-1">
+//             <span className="text-[#3D1562]/40 line-through text-xs sm:text-sm lg:text-sm xl:text-base">
+//               {plan.oldPrice}
+//             </span>
+//             <span className="bg-gradient-to-r from-[#FFDFA9]/60 to-[#BF9CF9]/80 rounded-2xl text-white px-1.5 sm:px-2 py-0.5 text-[9px] sm:text-xs lg:text-sm xl:text-sm font-semibold">
+//               {plan.discount}
+//             </span>
+//           </div>
+//         )}
+//       </div>
+
+//       {/* Title & Credits */}
+//       <h3 className="text-sm my-4 sm:text-base md:text-lg lg:text-xl xl:text-3xl font-bold text-[#3F1562] mb-0.5 sm:mb-1">
+//         {plan.name}
+//       </h3>
+//       <p className="text-[#3D1562]/50 mb-3 sm:mb-4 text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-base">
+//         {plan.credits} credits → {plan.profiles} profiles
+//       </p>
+
+//       {/* Features */}
+//       <div className="space-y-1.5 sm:space-y-2 mb-4 sm:mb-6">
+//         {plan.features.map((feature: any) => (
+//           <div key={feature} className="flex items-start">
+//             <span className="mr-1.5 sm:mr-2 mt-1 flex-shrink-0 text-bg">
+//               <svg
+//                 width="11"
+//                 height="8"
+//                 viewBox="0 0 13 10"
+//                 fill="none"
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 className="sm:w-[13px] sm:h-[10px]"
+//               >
+//                 <path
+//                   d="M11.9391 1.43858L5.96981 9.64444C5.82745 9.83578 5.61517 9.96115 5.38066 9.99238C5.14616 1.0236 4.9091 9.95808 4.72276 9.81052L0.460123 6.35821C0.0839729 6.05328 0.023065 5.4972 0.324082 5.11616C0.625098 4.73512 1.17405 4.67342 1.5502 4.97835L5.10472 7.8591L10.5351 0.393523C10.7131 0.122841 11.0236 -0.0268124 11.3433 0.00397824C11.663 0.0347689 11.9401 0.241022 12.0646 0.540849C12.1891 0.840676 12.1409 1.18556 11.9391 1.43858Z"
+//                   fill="#C55A83"
+//                 />
+//               </svg>
+//             </span>
+//             <span className="text-[#3D1562] font-semibold text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-base opacity-80">
+//               {feature}
+//             </span>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+
+//     {/* CTA Button */}
+//     <div>
+//       <button className="w-full px-4 sm:px-6 py-2 rounded-lg font-medium text-xs sm:text-sm lg:text-sm xl:text-base bg-gradient-to-r from-yellow-800 via-purple-800 to-pink-500 bg-clip-text text-transparent border-[2px] border-[#3d156236] transition duration-300 ease-in-out hover:shadow-[0_0_15px_rgba(236,72,153,0.6)] hover:border-purple-50">
+//         Choose plan
+//       </button>
+//     </div>
+//   </div>
+// ))}
+
+// </div>
+
+
+
+//     </CommonModal>
+//   );
+// }
